7a18b560e5cb12404d0b8d8430b7c74dd1306088
Hilmar Birgir Ólafsson <hbo3@hi.is>
2012-07-24 22:24:09 +0000

    fyrsta útgáfa

--- remindme/views.py
+++ remindme/views.py
@@ -1,10 +1,6 @@
 # encoding=utf-8
 
-import datetime, locale, json
-
-from jinja2 import TemplateNotFound
-from flask import (render_template, request, session, flash, 
-                   redirect, abort, url_for, current_app, jsonify)
+from flask import render_template
 
 from .app import app
 from .tumblr import tumblr
@@ -12,11 +8,17 @@ from .twitter import twitter
 
 api_key = 'GsoVsAmVFvxGehP0kN2s6fs3DsoNfpAR19bdcTuk0F2PCpGswf'
 
+
 @app.route('/', methods=['GET'])
 def index():
-    blogs, tumblr_dates, titles = tumblr('eskitech',1,api_key)
-    tweets, twitter_dates = twitter('EskiTech',1)
-    return render_template('base.html',blogs=blogs,titles=titles, tumblr_dates=tumblr_dates, tweets=tweets,twitter_dates=twitter_dates)
+    blogs, tumblr_dates, titles = tumblr('eskitech', 1, api_key)
+    tweets, twitter_dates = twitter('EskiTech', 1)
+    return render_template('index.html', blogs=blogs, titles=titles, tumblr_dates=tumblr_dates, tweets=tweets, twitter_dates=twitter_dates)
 
 
+@app.route('/blog', methods=['GET'])
+def blogs():
+    blogs, tumblr_dates, titles = tumblr('eskitech', 2, api_key)
+    tweets, twitter_dates = twitter('EskiTech', 1)
+    return render_template('blogs.html', blogs=blogs, titles=titles, tumblr_dates=tumblr_dates, tweets=tweets, twitter_dates=twitter_dates)
 
